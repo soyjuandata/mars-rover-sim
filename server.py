022197cfcb42e573b4bd78c04048dacb8e803b1c
soyjuandata <bocanegrajuan1031@gmail.com>
2026-09-19 10:44:12 -0500

    agrego print

--- server.py
+++ server.py
@@ -410,5 +410,6 @@ def get_camera(format: Literal["json", "raw"] = Query("json")) -> Response:
 
 # --------------------------------------------------------------------------- #
 if __name__ == "__main__":
+    print('hola')
     print("Simulador de rover marciano -> http://127.0.0.1:8000")
     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="warning")
